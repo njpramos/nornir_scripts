5a7f140c9721f647fe82b57368932bf94cb93d0b
NJ Ramos <njpramos@gmail.com>
2023-04-12 14:19:39 +0100

    12-04-23 update

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import sys
 import getpass
+import pprint
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
@@ -10,13 +11,21 @@ nr = InitNornir(config_file="inventory/config.yaml")
 password = getpass.getpass()
 
 nr.inventory.defaults.password = password
+ 
+def show_command(task):
 
-# parse the username in the CLI
+    show_result = task.run(task=send_command, command="show ip interface brief")
+    task.host["facts"] = show_result.scrapli_response.genie_parse_output()
 
-# username = sys.argv[1]
 
-# nr.inventory.defaults.username = username
+    interfaces = task.host["facts"]["interface"]
 
-results = nr.run(task=send_command, command="show run")
 
-print_result(results)
+    for interface in interfaces:
+
+        link_status = interfaces[interface]['protocol']
+        
+        if link_status == 'up':
+            print(f"{interface} is UP")
+
+results = nr.run(task=show_command)

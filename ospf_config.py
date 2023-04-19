babd84139b58f9b276f723b5b89ac67caef43e02
NJ Ramos <njpramos@gmail.com>
2023-04-19 15:39:11 +0100

    updated 19-04-2023

---
+++ ospf_config.py
@@ -0,0 +1,16 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_configs
+from nornir_utils.plugins.functions import print_result
+from nornir_jinja2.plugins.tasks import template_file
+
+nr = InitNornir(config_file="ospf_inventory/config.yaml")
+
+def send_ospf_configs(task):
+    template = task.run(task=template_file, template="ospf.j2", path="templates")
+    task.host["ospf_config"] = template.result
+    rendered = task.host["ospf_config"]
+    configuration = rendered.splitlines()
+    task.run(task=send_configs, configs=configuration)
+
+results = nr.run(task=send_ospf_configs)
+print_result(results)
